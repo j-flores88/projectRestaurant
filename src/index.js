6c0a9848ce66370f96bb2e525c6cb84917935df4
Joel Flores <j_flores88@outlook.com>
2021-07-15 18:18:24 -0700

    Add about page, make pages clickable

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import { navBar } from './navBar';
 import { welcomeTitle, subTitle } from "./welcome";
 import { footerElement } from "./footer";
 import { finalMenu } from "./menu";
+import { aboutHeader, aboutUs } from "./about";
 
 const content = document.getElementById('content');
 
 const homeScreen = () => {
+    content.innerHTML = '';
     renderOnPage('nav', navBar)
-    // renderOnPage('main', welcomeTitle)
-    // renderOnPage('main', subTitle)
+    renderOnPage('main', welcomeTitle)
+    renderOnPage('main', subTitle)
+    renderOnPage('footer', footerElement)
+}
+
+const showMenu = () => {
+    content.innerHTML = '';
+    renderOnPage('nav', navBar)
     renderOnPage('main', finalMenu)
     renderOnPage('footer', footerElement)
 }
+
+const showAboutUs = () => {
+    content.innerHTML = '';
+    renderOnPage('nav', navBar)
+    renderOnPage('main', aboutHeader)
+    renderOnPage('main', aboutUs)
+    renderOnPage('footer', footerElement)
+}
+
+document.addEventListener('click', (e) => {
+    const target = e.target.innerText;
+    if(target === 'HOME') homeScreen();
+    if(target === 'MENU') showMenu();
+    if(target === 'ABOUT') showAboutUs();
+})
 
 homeScreen();
 
